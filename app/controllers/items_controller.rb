3bc04f310694bba3e1819965dfe264ac0a04fdca
a-motomura <blackuriboh@gmail.com>
2020-09-14 15:08:20 +0900

    販売価格の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,19 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.order("created_at DESC")
   end
   
   def new
     @items = Item.all
   end
 
+  def create
+    Item.create(item_params)
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
