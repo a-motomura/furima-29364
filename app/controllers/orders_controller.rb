472f7cf1fc0bfa6eab35bd8701b7932a392f7242
a-motomura <blackuriboh@gmail.com>
2020-09-24 16:07:03 +0900

    不備修正１

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,7 +25,7 @@ class OrdersController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == @item.user_id
+    redirect_to root_path if current_user.id == @item.user_id || @item.users_item
   end
 
   def pay_item
